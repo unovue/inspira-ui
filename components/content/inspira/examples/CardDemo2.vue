0ab598deecb8f5530d18ed8c9608dfb837b95aee
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-06 14:35:08 +0530

    Added NuxtImage

--- components/content/inspira/examples/CardDemo2.vue
+++ components/content/inspira/examples/CardDemo2.vue
@@ -23,7 +23,7 @@
           :rotate-z="-10"
           class="w-full mt-4"
         >
-          <img
+          <NuxtImage
             src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             height="1000"
             width="1000"
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   components: [
     {
       path: "~/components",
       pathPrefix: false,
     },
   ],
+
   extends: ["shadcn-docs-nuxt"],
   compatibilityDate: "2024-07-06",
+  modules: ["@nuxt/image"],
 });
