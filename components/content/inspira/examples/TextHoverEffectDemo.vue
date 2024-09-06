3e83032cdbe7b479b234a8b41cb1815971f594de
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-06 14:19:05 +0530

    Updated import

--- components/content/inspira/examples/TextHoverEffectDemo.vue
+++ components/content/inspira/examples/TextHoverEffectDemo.vue
@@ -3,11 +3,11 @@
     <div
       className="h-auto flex min-md:flex-1 max-lg:w-full items-center justify-center"
     >
-      <TextHover class="w-[90%]" text="INSPIRA" />
+      <TextHoverEffect class="w-[90%]" text="INSPIRA" />
     </div>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
-import { TextHover } from "~/components/content/inspira/ui/text-hover-effect/";
+import { TextHoverEffect } from "~/components/content/inspira/ui/text-hover-effect";
 </script>
