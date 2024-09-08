bf7596e8f416cfa87a0b50fd0a5ddbdb3ba3d4ff
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-08 21:27:11 +0530

    Removed content plolicy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "nuxt-gtag", "nuxt-security"],
-  security: {
-    headers: {
-      contentSecurityPolicy:  {
-          'default-src': ["'self'"],
-          'script-src': ["'self'", "'wasm-unsafe-eval'"],
-      },
-    },
-  },
+  modules: ["@nuxt/image", "nuxt-gtag"],
 
   components: [
     {
