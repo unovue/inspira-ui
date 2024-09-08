3c768326725ac45d3bb182fcf45ccea60de94b22
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-08 22:20:09 +0530

    Updated CSP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "nuxt-gtag"],
+  modules: ["@nuxt/image", "nuxt-gtag", "nuxt-security"],
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        'script-src': ["*", "'unsafe-inline'", "'unsafe-eval'"],
+      },
+    },
+  },
+
+  image: {
+    domains: ["images.unsplash.com"],
+  },
 
   components: [
     {
