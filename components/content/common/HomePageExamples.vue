e1a1f4a69976bbd33a00a92d534bb118905a5f97
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-13 13:46:24 +0530

    Added radiant text effect component

--- components/content/common/HomePageExamples.vue
+++ components/content/common/HomePageExamples.vue
@@ -22,15 +22,26 @@
         </h3>
         <CardDemo2 />
       </DotBackground>
+
+      <DotBackground class="py-8 px-4 border rounded-lg flex flex-col h-fit">
+        <h3
+          class="text-3xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8"
+        >
+          Text Hover Effect
+        </h3>
+        <p>Hover below to reveal text</p>
+        <TextHoverEffectDemo />
+      </DotBackground>
+
       <DotBackground
         class="py-8 px-4 border rounded-lg flex flex-col gap-4 h-fit"
       >
         <h3
           class="text-3xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8"
         >
-          Text Hover Effect
+          Radiant Text
         </h3>
-        <TextHoverEffectDemo />
+        <RadiantTextDemo />
       </DotBackground>
     </div>
   </ClientOnly>
---
+++ components/content/inspira/examples/RadiantTextDemo.vue
@@ -0,0 +1,8 @@
+<template>
+  <RadiantText
+    class="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400"
+    :duration="5"
+  >
+    <span class="text-3xl font-bold">✨ Introducing Inspira UI</span>
+  </RadiantText>
+</template>
