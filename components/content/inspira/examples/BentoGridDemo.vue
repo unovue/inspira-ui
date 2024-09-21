a967714b5d4c22102a31333eef8c41695a1da582
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-21 13:46:51 +0530

    Added bento grid docs and demo

---
+++ components/content/inspira/examples/BentoGridDemo.vue
@@ -0,0 +1,59 @@
+<template>
+  <BentoGrid class="max-w-4xl mx-auto">
+    <BentoGridItem
+      v-for="(item, index) in items"
+      :key="index"
+      :class="index === 3 || index === 6 ? 'md:col-span-2' : ''"
+    >
+      <template #header>
+        <div class="animate-pulse flex space-x-4 w-full h-full">
+          <div class="rounded-md flex flex-1 h-full w-full bg-zinc-800"></div>
+        </div>
+      </template>
+
+      <template #title>
+        <strong>{{ item.title }}</strong>
+      </template>
+
+      <template #icon> </template>
+
+      <template #description>
+        <p>{{ item.description }}</p>
+      </template>
+    </BentoGridItem>
+  </BentoGrid>
+</template>
+
+<script lang="ts" setup>
+const items = [
+  {
+    title: "The Dawn of Innovation",
+    description: "Explore the birth of groundbreaking ideas and inventions.",
+  },
+  {
+    title: "The Digital Revolution",
+    description: "Dive into the transformative power of technology.",
+  },
+  {
+    title: "The Art of Design",
+    description: "Discover the beauty of thoughtful and experience design.",
+  },
+  {
+    title: "The Power of Communication",
+    description:
+      "Understand the impact of effective communication in our lives.",
+  },
+  {
+    title: "The Pursuit of Knowledge",
+    description: "Join the quest for understanding and enlightenment.",
+  },
+  {
+    title: "The Joy of Creation",
+    description: "Experience the thrill of bringing ideas to life.",
+  },
+  {
+    title: "The Spirit of Adventure",
+    description: "Embark on exciting journeys and thrilling discoveries.",
+  },
+];
+</script>
---
+++ content/2.components/bento-grid.md
@@ -0,0 +1,31 @@
+---
+title: Bento Grid
+description: A grid layout with title, description, icon and a header component.
+---
+
+::ComponentLoader{label="Preview" componentName="BentoGridDemo" type="examples"}
+::
+
+## API
+
+| Slot Name     | Description                       |
+| ------------- | --------------------------------- |
+| `title`       | Component to show as title.       |
+| `description` | Component to show as description. |
+| `icon`        | Component to show as icon.        |
+| `header`      | Component to show as header.      |
+
+## Component Code
+
+You can copy and paste the following code to create these components:
+
+::CodeGroup
+::CodeViewerTab{label="BentoGrid.vue" language="vue" componentName="BentoGrid" type="ui" id="bento-grid"}
+::
+::CodeViewerTab{label="BentoGridItem.vue" language="vue" componentName="BentoGridItem" type="ui" id="bento-grid"}
+::
+::
+
+## Credits
+
+- Credits to [Aceternity UI](https://ui.aceternity.com/components/bento-grid) for this fantastic component.
