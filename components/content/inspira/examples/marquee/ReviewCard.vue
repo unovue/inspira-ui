277015b3dc040ec39c6dcda101d8620acfeb150b
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-26 18:45:32 +0530

    Added Marquee Demo

---
+++ components/content/inspira/examples/marquee/ReviewCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <figure
+    class="relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4 border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+  >
+    <div class="flex flex-row items-center gap-2">
+      <NuxtImg :src="img" class="rounded-full" width="32" height="32" alt="" />
+      <div class="flex flex-col">
+        <figcaption class="text-sm font-medium dark:text-white">
+          {{ name }}
+        </figcaption>
+        <p class="text-xs font-medium dark:text-white/40">{{ username }}</p>
+      </div>
+    </div>
+    <blockquote class="mt-2 text-sm">{{ body }}</blockquote>
+  </figure>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  img: { type: String, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true },
+  body: { type: String, required: true },
+});
+</script>
