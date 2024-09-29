346b83cec07111196d5d4e33437536dfc5e07e94
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-29 11:56:28 +0530

    Added Card Spotlight component.

---
+++ components/content/inspira/ui/card-spotlight/CardSpotlight.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    @mousemove="handleMouseMove"
+    @mouseleave="handleMouseLeave"
+    :class="[
+      'group relative flex w-full h-full overflow-hidden rounded-xl bg-neutral-100 dark:bg-neutral-900 border text-black dark:text-white',
+      $props.class,
+    ]"
+  >
+    <div class="relative z-10">
+      <slot></slot>
+    </div>
+    <div
+      class="pointer-events-none absolute inset-0 rounded-xl opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+      :style="{
+        background: backgroundStyle,
+        opacity: gradientOpacity,
+      }"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted } from "vue";
+
+const props = defineProps({
+  gradientSize: { type: Number, default: 200 },
+  gradientColor: { type: String, default: "#262626" },
+  gradientOpacity: { type: Number, default: 0.8 },
+  class: { type: String, default: "" },
+});
+
+const mouseX = ref(-props.gradientSize * 10);
+const mouseY = ref(-props.gradientSize * 10);
+
+const handleMouseMove = (e: MouseEvent) => {
+  const target = e.currentTarget as HTMLElement;
+  const rect = target.getBoundingClientRect();
+  mouseX.value = e.clientX - rect.left;
+  mouseY.value = e.clientY - rect.top;
+};
+
+const handleMouseLeave = () => {
+  mouseX.value = -props.gradientSize * 10;
+  mouseY.value = -props.gradientSize * 10;
+};
+
+onMounted(() => {
+  mouseX.value = -props.gradientSize * 10;
+  mouseY.value = -props.gradientSize * 10;
+});
+
+const backgroundStyle = computed(() => {
+  return `radial-gradient(
+    circle at ${mouseX.value}px ${mouseY.value}px,
+    ${props.gradientColor} 0%,
+    rgba(0, 0, 0, 0) 70%
+  )`;
+});
+</script>
