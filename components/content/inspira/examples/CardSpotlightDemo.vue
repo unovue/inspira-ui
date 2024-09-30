99f0f893c61422c72ca54175fb9bff984ba71838
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-09-30 11:57:46 +0530

    Added Card Spotlight Demo and Docs

---
+++ components/content/inspira/examples/CardSpotlightDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex h-[500px] w-full flex-col gap-4 lg:h-[250px] lg:flex-row">
+    <CardSpotlight
+      class="cursor-pointer flex-col items-center justify-center shadow-2xl whitespace-nowrap text-4xl"
+      :gradientColor="isDark ? '#363636' : '#C9C9C9'"
+    >
+      Card Spotlight
+    </CardSpotlight>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isDark = computed(() => useColorMode().value == "dark");
+</script>
