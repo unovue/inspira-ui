eeaf87dca898f1185ef200bb8bed641853c32949
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-10-11 11:09:06 +0530

    Added Shimmer Button

---
+++ components/content/inspira/examples/ShimmerButtonDemo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="z-10 flex min-h-64 items-center justify-center">
+    <ShimmerButton class="shadow-2xl" shimmerSize="2px">
+      <span
+        class="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg"
+      >
+        Cool Button
+      </span>
+    </ShimmerButton>
+  </div>
+</template>
