78aa0b9c9bc812de9be50a681ad3499eca4825a7
Craig Riley <hello@craigriley.uk>
2024-10-13 11:50:13 +0100

    Squashed commit of the following:
    commit 09d1d6f0d820da1ea0ae45d24935528b2b755877
    Author: Craig Riley <hello@craigriley.uk>
    Date:   Sun Oct 13 11:38:25 2024 +0100
    
        feat: add ImagesSlider component
    
        commit a92620226a2ef1abe70f66f3d38ee45e0d70966f
        Author: Craig Riley <hello@craigriley.uk>
        Date:   Sun Oct 13 11:25:10 2024 +0100
    
            Add name to credits and cleanup
    
        commit 48d4a6c770a94911f6283dcc99d53f070bbd2899
        Author: Craig Riley <hello@craigriley.uk>
        Date:   Sat Oct 12 16:35:40 2024 +0100
    
            feat: add ImagesSlider component

---
+++ components/content/inspira/examples/images-slider/ImagesSliderDemo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ImagesSlider :images="images" autoplay>
+      <div
+        class="w-full h-full flex justify-center items-end p-8 bg-gradient-to-b from-transparent via-transparent to-black/80"
+      >
+        <div class="max-w-prose text-center">
+          <h1 class="text-5xl mb-8">
+            The hero section slide-show nobody asked for
+          </h1>
+          <button
+            class="rounded-full backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white px-4 py-2"
+          >
+            Join Now
+          </button>
+        </div>
+      </div>
+    </ImagesSlider>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const images = [
+  "https://picsum.photos/seed/waiting/1920/1080",
+  "https://picsum.photos/seed/full-collapse/1920/1080",
+  "https://picsum.photos/seed/five-stories-falling/1920/1080",
+  "https://picsum.photos/seed/war-all-the-time/1920/1080",
+  "https://picsum.photos/seed/a-city-by-the-light-divided/1920/1080",
+  "https://picsum.photos/seed/common-existence/1920/1080",
+  "https://picsum.photos/seed/no-devolucion/1920/1080",
+];
+</script>
