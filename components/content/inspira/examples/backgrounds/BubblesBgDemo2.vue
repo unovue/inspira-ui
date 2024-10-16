e7717792de1a4da7a776c87f4279ea13cd4d6836
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-10-16 12:02:56 +0530

    Added bubbles background component

---
+++ components/content/inspira/examples/backgrounds/BubblesBgDemo2.vue
@@ -0,0 +1,11 @@
+<template>
+  <BubblesBg class="w-full h-96" :blur="4">
+    <div class="flex flex-col w-full h-full items-center justify-center">
+      <span
+        class="text-6xl text-background font-heading font-bold backdrop-blur-md opacity-80"
+      >
+        Hello
+      </span>
+    </div>
+  </BubblesBg>
+</template>
