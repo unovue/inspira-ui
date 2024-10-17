2d70a10da757bbe444402e130bfb8f73d81d75ea
Prem <premdasvm8@gmail.com>
2024-10-17 14:52:05 +0530

    feat: add HyperText component

---
+++ components/content/inspira/examples/HyperTextDemo.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex flex-col items-center justify-center space-y-4">
+        <HyperText text="Hyper Text" class="text-4xl font-bold" :duration="800" />
+    </div>
+</template>
