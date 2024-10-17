d55f3853f099dc8d78457aa7adb8226a3bae2a52
atif0075 <chatif476@gmail.com>
2024-10-17 19:03:18 +0500

    refactor: add prop for stars count & optimize the code

--- components/content/inspira/ui/text-reveal-card/TextRevealStars.vue
+++ components/content/inspira/ui/text-reveal-card/TextRevealStars.vue
@@ -1,41 +1,54 @@
 <template>
   <div class="absolute inset-0">
     <Motion
-      v-for="i in 130"
+      v-for="i in starsCount"
       :key="`star-${i}`"
       is="span"
-      :initial="{
-        top: `calc(${random() * 100}% + ${randomMove()}px)`,
-        left: `calc(${random() * 100}% + ${randomMove()}px)`,
-      }"
-      :enter="{
-        top: `calc(${random() * 100}% + ${randomMove()}px)`,
-        left: `calc(${random() * 100}% + ${randomMove()}px)`,
-        opacity: randomOpacity(),
-        scale: [1, 1.2, 0],
-        transition: {
-          opacity: {
-            duration: 1000,
-            repeat: Infinity,
-            type: 'tween',
-          },
-          scale: {
-            duration: 1000,
-            repeat: Infinity,
-            type: 'tween',
-          },
-          repeat: Infinity,
-          ease: 'linear',
-        },
-      }"
-      :duration="random() * 10000 + 20000"
+      :initial="generatePosition()"
+      :enter="generateEnterAnimation()"
+      :duration="randomDuration"
       class="inline-block absolute w-0.5 h-0.5 bg-white rounded-full z-[1]"
     />
   </div>
 </template>
 
 <script setup lang="ts">
+interface Props {
+  starsCount: number;
+}
+withDefaults(defineProps<Props>(), {
+  starsCount: 130,
+});
+
 const randomMove = () => Math.random() * 4 - 2;
 const randomOpacity = () => Math.random();
 const random = () => Math.random();
+
+const generatePosition = () => ({
+  top: `calc(${random() * 100}% + ${randomMove()}px)`,
+  left: `calc(${random() * 100}% + ${randomMove()}px)`,
+});
+
+const generateEnterAnimation = () => ({
+  top: `calc(${random() * 100}% + ${randomMove()}px)`,
+  left: `calc(${random() * 100}% + ${randomMove()}px)`,
+  opacity: randomOpacity(),
+  scale: [1, 1.2, 0],
+  transition: {
+    opacity: {
+      duration: 1000,
+      repeat: Infinity,
+      type: "tween",
+    },
+    scale: {
+      duration: 1000,
+      repeat: Infinity,
+      type: "tween",
+    },
+    repeat: Infinity,
+    ease: "linear",
+  },
+});
+
+const randomDuration = random() * 10000 + 20000;
 </script>
