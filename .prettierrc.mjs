7828f8ee3d2ed56581654213752ceeff95f92ed1
Prem <premdasvm8@gmail.com>
2024-10-18 11:52:54 +0530

    feat: add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+export default {
+  semi: true,
+  singleQuote: false,
+  printWidth: 80,
+  trailingComma: "all"
+};
