4fcc5e9caafd3c059fba626dfd2c17d603788940
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-10-21 16:52:03 +0200

    Fix formatting + import

--- components/content/inspira/ui/text-generate-effect/TextGenerateEffect.vue
+++ components/content/inspira/ui/text-generate-effect/TextGenerateEffect.vue
@@ -1,5 +1,5 @@
 <template>
-	<div :class="cn('leading-snug tracking-wide', props.class)">
+	<div :class="cn("leading-snug tracking-wide", props.class)">
 		<div ref="scope">
 			<span
 				v-for="(word, idx) in wordsArray"
@@ -13,36 +13,36 @@
 </template>
 
 <script setup lang="ts">
-import { computed, HTMLAttributes, onMounted, ref } from 'vue';
+import { computed, type HTMLAttributes, onMounted, ref } from "vue";
 
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 
 const props = withDefaults(
 	defineProps<{
 		words: string;
 		filter?: boolean;
 		duration?: number;
-		class: HTMLAttributes['class'];
+		class: HTMLAttributes["class"];
 	}>(),
 	{ duration: 0.7, filter: true },
 );
 
 const scope = ref(null);
-const wordsArray = computed(() => props.words.split(' '));
+const wordsArray = computed(() => props.words.split(" "));
 
 const spanStyle = computed(() => ({
 	opacity: 0,
-	filter: props.filter ? 'blur(10px)' : 'none',
+	filter: props.filter ? "blur(10px)" : "none",
 	transition: `opacity ${props.duration}s, filter ${props.duration}s`,
 }));
 
 onMounted(() => {
 	if (scope.value) {
-		const spans = (scope.value as HTMLElement).querySelectorAll('span');
+		const spans = (scope.value as HTMLElement).querySelectorAll("span");
 		spans.forEach((span: HTMLElement, index: number) => {
 			setTimeout(() => {
-				span.style.opacity = '1';
-				span.style.filter = props.filter ? 'blur(0px)' : 'none';
+				span.style.opacity = "1";
+				span.style.filter = props.filter ? "blur(0px)" : "none";
 			}, index * 200);
 		});
 	}
