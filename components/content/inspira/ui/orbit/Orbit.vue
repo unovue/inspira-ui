f1901a771ae0ddf37f58288a3645ae77ed711b85
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-10-21 17:32:24 +0200

    Clean code and fix props

--- components/content/inspira/ui/orbit/Orbit.vue
+++ components/content/inspira/ui/orbit/Orbit.vue
@@ -1,57 +1,68 @@
 <template>
-  <svg v-if="path" class="pointer-events-none absolute inset-0 size-full">
-    <circle
-      class="stroke-black/10 stroke-1 dark:stroke-white/10"
-      cx="50%"
-      cy="50%"
-      :r="radius"
-      fill="none"
-    />
-  </svg>
-  <div
-    :style="{
-      '--duration': duration,
-      '--radius': radius,
-      '--delay': -delay,
-      '--direction': reverse ? 'reverse' : 'normal',
-    }"
-    :class="
-      cn(
-        'absolute flex size-full transform-gpu animate-orbit items-center justify-center rounded-full border bg-black/10 dark:bg-white/10',
-        $props.class,
-      )
-    "
-  >
-    <slot />
-  </div>
+	<svg
+		v-if="path"
+		class="pointer-events-none absolute inset-0 size-full">
+		<circle
+			class="stroke-1 stroke-foreground/20"
+			cx="50%"
+			cy="50%"
+			:r="radius"
+			fill="none" />
+	</svg>
+	<div
+		:style="{
+			'--duration': duration,
+			'--radius': radius,
+			'--delay': -delay,
+			'--direction': reverse ? 'reverse' : 'normal',
+		}"
+		:class="
+			cn(
+				'absolute flex size-full transform-gpu animate-orbit',
+				props.class,
+			)
+		">
+		<slot />
+	</div>
 </template>
 
 <script lang="ts" setup>
-import { cn } from "~/lib/utils";
+import { cn } from '@/lib/utils';
 
-const props = defineProps({
-  class: String,
-  reverse: { type: Boolean, default: false },
-  duration: { type: Number, default: 20 },
-  delay: { type: Number, default: 10 },
-  radius: { type: Number, default: 50 },
-  path: { type: Boolean, default: true },
-});
+interface Props {
+		class?: string;
+		reverse?: boolean;
+		duration?: number;
+		delay?: number;
+		radius?: number;
+		path?: boolean;
+	};
+
+const props = withDefaults(
+	defineProps<Props>(),
+	{
+		reverse: false,
+		duration: 20,
+		delay: 10,
+		radius: 50,
+		path: true,
+	},
+);
 </script>
 
 <style scoped>
 @keyframes orbit {
-  0% {
-    transform: rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg);
-  }
+	0% {
+		transform: rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg);
+	}
+	100% {
+		transform: rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg);
+	}
 }
 
 .animate-orbit {
-  animation: orbit calc(var(--duration) * 1s) linear infinite;
-  animation-delay: calc(var(--delay) * 1s);
-  animation-direction: var(--direction);
+	animation: orbit calc(var(--duration) * 1s) linear infinite;
+	animation-delay: calc(var(--delay) * 1s);
+	animation-direction: var(--direction);
 }
 </style>
