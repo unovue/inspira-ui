d08439f89ca8eeaad4a1d22d0b140bc717cc086a
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-10-23 14:52:09 +0200

    Add earth at center (because it's an orbit ... space reference ...)

--- components/content/inspira/examples/OrbitDemo.vue
+++ components/content/inspira/examples/OrbitDemo.vue
@@ -8,9 +8,9 @@ import { ORBIT_DIRECTION } from '../ui/orbit';
     class="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"
   >
     <span
-      class="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-black"
+      class="pointer-events-none text-center text-8xl font-semibold leading-none"
     >
-      Orbit
+      🌍
     </span>
 
     <!-- Inner Circles -->
