5db4c8b49cd82ff49af030e89d522bd0e8515134
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2024-10-24 16:47:48 +0200

    Add component + demo + doc page

---
+++ components/content/inspira/examples/text-highlight/TextHighlightDemo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center text-blance p-10">
+    <h1 class="text-4xl font-bold">You are a <TextHighlight text="fucking great developer" class="bg-gradient-to-r from-indigo-300 to-purple-300 ">!</TextHighlight></h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ components/content/inspira/ui/text-highlight/TextHighlight.vue
@@ -0,0 +1,36 @@
+<template>
+  <span
+    :class="cn('relative inline-block pb-1 px-2 rounded-lg', props.class)"
+    >{{ text }}</span
+  >
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/lib/utils';
+
+interface Props {
+  text: string;
+  class: HTMLAttributes['class'];
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+@keyframes backgroundExpand {
+  0% {
+    background-size: 0% 100%;
+  }
+  100% {
+    background-size: 100% 100%;
+  }
+}
+
+span {
+  background-size: 0% 100%;
+  background-repeat: no-repeat;
+  background-position: left center;
+  animation: backgroundExpand ease-in-out 2s forwards;
+}
+</style>
