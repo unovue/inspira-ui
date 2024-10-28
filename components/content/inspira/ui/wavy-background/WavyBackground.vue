aee7ba5c0a3b8bdd5dfef5a957f3502ff9ac76a7
Reddy Uppathi <popvictoria@gmail.com>
2024-10-29 03:38:32 +0530

    feat: add WavyBackground component

---
+++ components/content/inspira/ui/wavy-background/WavyBackground.vue
@@ -0,0 +1,117 @@
+<template>
+  <div :class="cn('h-screen flex flex-col items-center justify-center', props.containerClass)">
+    <canvas
+      ref="canvasRef"
+      class="absolute z-0"
+      id="canvas"
+      :style="{ filter: isSafari ? `blur(${props.blur}px)` : undefined }"
+    ></canvas>
+    <div :class="cn('relative z-10', props.class)">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { createNoise3D } from "simplex-noise";
+import { cn } from "@/lib/utils";
+
+interface WavyBackgroundProps {
+  class?: string;
+  containerClass?: string;
+  colors?: string[];
+  waveWidth?: number;
+  backgroundFill?: string;
+  blur?: number;
+  speed?: "slow" | "fast";
+  waveOpacity?: number;
+  [key: string]: any;
+}
+
+const props = withDefaults(defineProps<WavyBackgroundProps>(), {
+  colors: () => ["#38bdf8", "#818cf8", "#c084fc", "#e879f9", "#22d3ee"],
+  waveWidth: 50,
+  backgroundFill: "black",
+  blur: 10,
+  speed: "fast",
+  waveOpacity: 0.5,
+});
+
+const noise = createNoise3D();
+
+// Declare variables with null
+let w: number,
+  h: number,
+  nt = 0,
+  ctx: CanvasRenderingContext2D | null = null;
+
+const canvasRef = useTemplateRef<HTMLCanvasElement | null>("canvasRef");
+
+const getSpeed = (): number => {
+  return props.speed === "slow" ? 0.001 : 0.002;
+};
+
+const init = () => {
+  const canvas = canvasRef.value;
+  if (canvas) {
+    ctx = canvas.getContext("2d");
+    if (ctx) {
+      const parent = canvasRef.value.parentElement;
+      if (parent) {
+        w = ctx.canvas.width = parent.clientWidth;
+        h = ctx.canvas.height = parent.clientHeight;
+      }
+
+      ctx.filter = `blur(${props.blur}px)`;
+      window.onresize = () => {
+        if (parent) {
+          w = ctx!.canvas.width = parent.clientWidth;
+          h = ctx!.canvas.height = parent.clientHeight;
+        }
+        ctx!.filter = `blur(${props.blur}px)`;
+      };
+      render();
+    }
+  }
+};
+
+const drawWave = (n: number) => {
+  nt += getSpeed();
+  for (let i = 0; i < n; i++) {
+    ctx!.beginPath();
+    ctx!.lineWidth = props.waveWidth!;
+    ctx!.strokeStyle = props.colors[i % props.colors!.length];
+    for (let x = 0; x < w; x += 5) {
+      const y = noise(x / 800, 0.3 * i, nt) * 100;
+      ctx!.lineTo(x, y + h * 0.5); // Adjust for height, at 50% of the container
+    }
+    ctx!.stroke();
+    ctx!.closePath();
+  }
+};
+
+let animationId: number;
+const render = () => {
+  if (ctx) {
+    ctx.fillStyle = props.backgroundFill!;
+    ctx.globalAlpha = props.waveOpacity!;
+    ctx.fillRect(0, 0, w, h);
+    drawWave(5);
+    animationId = requestAnimationFrame(render);
+  }
+};
+
+onBeforeUnmount(() => {
+  cancelAnimationFrame(animationId);
+});
+
+const isSafari = ref(false);
+onMounted(() => {
+  isSafari.value =
+    typeof window !== "undefined" &&
+    navigator.userAgent.includes("Safari") &&
+    !navigator.userAgent.includes("Chrome");
+
+  init();
+});
+</script>
