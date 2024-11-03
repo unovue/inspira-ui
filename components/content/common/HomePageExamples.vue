11f9033d4a57b542bb40258587607c267d86eeae
Thomas <twt@outlook.dk>
2024-11-03 23:34:58 +0100

    fix(formatting): Formatted examples back to original structure

--- components/content/common/HomePageExamples.vue
+++ components/content/common/HomePageExamples.vue
@@ -1,39 +1,56 @@
 <template>
   <div class="my-16 mt-24 flex flex-col items-center justify-center">
-    <BlurReveal :delay="0" :duration="1">
+    <BlurReveal
+      :delay="0"
+      :duration="1"
+    >
       <div class="text-5xl font-semibold">Check out some examples!</div>
     </BlurReveal>
   </div>
-  <div class="flex w-full max-w-7xl flex-col flex-wrap items-center justify-center gap-4 p-4 lg:flex-row">
-    <PatternBackground :animate="true" :direction="PATTERN_BACKGROUND_DIRECTION.TopRight"
+  <div
+    class="flex w-full max-w-7xl flex-col flex-wrap items-center justify-center gap-4 p-4 lg:flex-row"
+  >
+    <PatternBackground
+      :animate="true"
+      :direction="PATTERN_BACKGROUND_DIRECTION.TopRight"
       :variant="PATTERN_BACKGROUND_VARIANT.Dot"
       class="flex h-fit w-full flex-col items-center gap-4 rounded-lg border px-4 py-8 overflow-hidden"
-      :speed="PATTERN_BACKGROUND_SPEED.Slow">
+      :speed="PATTERN_BACKGROUND_SPEED.Slow"
+    >
       <h3
-        class="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-3xl font-bold text-transparent">
+        class="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-3xl font-bold text-transparent"
+      >
         3D Card Hover Effect
       </h3>
       <CardDemo2 />
     </PatternBackground>
 
-    <PatternBackground :animate="true" :direction="PATTERN_BACKGROUND_DIRECTION.TopRight"
+    <PatternBackground
+      :animate="true"
+      :direction="PATTERN_BACKGROUND_DIRECTION.TopRight"
       :variant="PATTERN_BACKGROUND_VARIANT.Dot"
       class="flex h-fit w-full flex-col items-center gap-4 rounded-lg border px-4 py-8 overflow-hidden"
-      :speed="PATTERN_BACKGROUND_SPEED.Slow">
+      :speed="PATTERN_BACKGROUND_SPEED.Slow"
+    >
       <h3
-        class="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-3xl font-bold text-transparent">
+        class="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-3xl font-bold text-transparent"
+      >
         Text Hover Effect
       </h3>
       <p>Hover below to reveal text</p>
       <TextHoverEffectDemo />
     </PatternBackground>
 
-    <PatternBackground :animate="true" :direction="PATTERN_BACKGROUND_DIRECTION.TopRight"
+    <PatternBackground
+      :animate="true"
+      :direction="PATTERN_BACKGROUND_DIRECTION.TopRight"
       :variant="PATTERN_BACKGROUND_VARIANT.Dot"
       class="flex h-fit w-full flex-col items-center gap-4 rounded-lg border px-4 py-8 overflow-hidden"
-      :speed="PATTERN_BACKGROUND_SPEED.Slow">
+      :speed="PATTERN_BACKGROUND_SPEED.Slow"
+    >
       <h3
-        class="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-3xl font-bold text-transparent">
+        class="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-3xl font-bold text-transparent"
+      >
         Radiant Text
       </h3>
       <RadiantTextDemo />
