a1906ed1b7e1cacd3dce4f074fb20898c5be6fdd
Grzegorz <greg.krol1996@gmail.com>
2024-11-04 23:08:21 +0100

    feat: Add Number Ticker component

---
+++ components/content/inspira/examples/NumberTickerDemo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex min-h-64 items-center justify-center">
+    <p
+      className="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-black dark:text-white"
+    >
+      <NumberTicker :value="100" />
+    </p>
+  </div>
+</template>
