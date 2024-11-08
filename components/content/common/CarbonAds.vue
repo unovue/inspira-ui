8447fe138e1bb882078816b667c12380c2cb5df2
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-11-08 18:18:37 +0530

    Updated CarbonAds

--- components/content/common/CarbonAds.vue
+++ components/content/common/CarbonAds.vue
@@ -1,10 +1,23 @@
 <template>
   <ScriptCarbonAds
-    class="max-w-sm gap-4 rounded-lg border border-gray-200 bg-white text-black dark:border-gray-800 dark:bg-white/5 dark:text-white"
+    :class="
+      cn(
+        'w-sm gap-4 rounded-lg border border-gray-200 bg-white text-black dark:border-gray-800 dark:bg-white/5 dark:text-white',
+        props.class,
+      )
+    "
     serve="CW7DEK37"
     placement="inspira-uicom"
   />
 </template>
+
+<script lang="ts" setup>
+import { cn } from "~/lib/utils";
+
+const props = defineProps({
+  class: String,
+});
+</script>
 
 <style>
 .carbon-poweredby {
--- components/content/common/ComponentLoader.vue
+++ components/content/common/ComponentLoader.vue
@@ -1,18 +1,21 @@
 <template>
-  <ClientOnly>
-    <CodeGroup>
-      <div
-        :label="label || componentName"
-        icon="lucide:laptop-minimal"
-      >
-        <ComponentViewer :component-name="componentName" />
-      </div>
-      <CodeViewerTab
-        v-bind="$props"
-        label="Code"
-      />
-    </CodeGroup>
-  </ClientOnly>
+  <div class="w-full space-y-8">
+    <ClientOnly>
+      <CodeGroup>
+        <div
+          :label="label || componentName"
+          icon="lucide:laptop-minimal"
+        >
+          <ComponentViewer :component-name="componentName" />
+        </div>
+        <CodeViewerTab
+          v-bind="$props"
+          label="Code"
+        />
+      </CodeGroup>
+    </ClientOnly>
+    <CarbonAds />
+  </div>
 </template>
 
 <script lang="ts" setup>
