c53fb6a5e8fd73bb131c13f58dc793a2862c8b16
atif0075 <chatif476@gmail.com>
2024-11-09 12:13:42 +0500

    chore: Update CompareDemo images

--- components/content/inspira/examples/compare/CompareDemo.vue
+++ components/content/inspira/examples/compare/CompareDemo.vue
@@ -4,8 +4,8 @@
       class="rounded-3xl border border-neutral-200 bg-neutral-100 p-4 dark:border-neutral-800 dark:bg-neutral-900"
     >
       <Compare
-        first-image="https://assets.aceternity.com/code-problem.png"
-        second-image="https://assets.aceternity.com/code-solution.png"
+        first-image="https://images.vscodethemes.com/antfu.theme-vitesse/vitesse-light-soft-js-preview-4JM.svg"
+        second-image="https://images.vscodethemes.com/antfu.theme-vitesse/vitesse-dark-soft-js-preview-4JM.svg"
         first-content-class="object-cover object-left-top rounded-xl overflow-hidden"
         second-content-class="object-cover object-left-top rounded-xl overflow-hidden"
         class="h-[250px] w-[200px] md:h-96 md:w-[500px]"
