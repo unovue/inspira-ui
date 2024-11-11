3c0fe02f82629a9538be571e95d9d620370b9cdf
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-11-11 12:25:50 +0530

    Updated names

--- components/content/inspira/examples/compare/CompareAutoplayDemo.vue
+++ components/content/inspira/examples/compare/CompareAutoplayDemo.vue
@@ -9,8 +9,8 @@
       class="mx-auto h-1/2 w-3/4 rounded-3xl border border-neutral-200 bg-neutral-100 p-1 md:h-3/4 md:p-4 dark:border-neutral-800 dark:bg-neutral-900"
     >
       <Compare
-        first-image="/images/inspira-light.png"
-        second-image="/images/inspira-dark.png"
+        first-image="/images/Inspira-light.png"
+        second-image="/images/Inspira-dark.png"
         first-content-class="object-cover object-left-center w-full h-96"
         second-content-class="object-cover object-left-center w-full h-96"
         class="size-full rounded-[22px] md:rounded-xl"
