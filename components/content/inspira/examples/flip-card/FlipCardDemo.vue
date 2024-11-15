c47d0f719b25aa92ecc0e0f6dac14463f2cdb03f
Reddy Uppathi <popvictoria@gmail.com>
2024-11-16 01:12:50 +0530

    feat: add FlipCard Component demo

---
+++ components/content/inspira/examples/flip-card/FlipCardDemo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center">
+    <FlipCard
+      description="Inspira UI offers beautifully designed, reusable animation components and includes custom components developed by us and contributed by the community."
+      image=" https://images.unsplash.com/photo-1525373698358-041e3a460346?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3"
+      rotate="y"
+      subtitle="What is Inspira UI?"
+      title="Inspira UI"
+    />
+  </div>
+</template>
