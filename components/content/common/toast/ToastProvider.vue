807b2fd55fd0f651dce1da4300f562588a434097
ZTL-UwU <zhangtianli2006@163.com>
2024-11-29 13:01:01 +0800

    feat: add toast component
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

---
+++ components/content/common/toast/ToastProvider.vue
@@ -0,0 +1,11 @@
+<template>
+  <ToastProvider v-bind="props">
+    <slot />
+  </ToastProvider>
+</template>
+
+<script setup lang="ts">
+import { ToastProvider, type ToastProviderProps } from "radix-vue";
+
+const props = defineProps<ToastProviderProps>();
+</script>
