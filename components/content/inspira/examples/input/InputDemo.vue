ea229cfe8e231453724956f07174a97c67096c88
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-11-30 11:00:00 +0530

    Added Input component

---
+++ components/content/inspira/examples/input/InputDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex h-56 w-full flex-col items-center justify-center gap-2">
+    <UiLabel
+      for="inputDemo"
+      class="ml-4 w-full max-w-sm"
+      >Hover over below input</UiLabel
+    >
+    <IInput
+      id="inputDemo"
+      placeholder="Hover over me"
+      container-class="w-full max-w-sm"
+    ></IInput>
+  </div>
+</template>
