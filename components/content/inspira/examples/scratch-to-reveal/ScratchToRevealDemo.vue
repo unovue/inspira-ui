7a4e338baa6f32245f4c3a12a5e722fada2409dc
whbbit1999 <luckyboy_dong@sina.com>
2024-12-20 18:35:58 +0800

    feat: Scratch To Reveal component, forked from MagicUI

---
+++ components/content/inspira/examples/scratch-to-reveal/ScratchToRevealDemo.vue
@@ -0,0 +1,18 @@
+<template>
+  <ScratchToReveal
+    :width="250"
+    :height="250"
+    :min-scratch-percentage="50"
+    class="mx-auto flex items-center justify-center overflow-hidden rounded-2xl border-2 bg-gray-100"
+    :gradient-colors="['#A97CF8', '#F38CB8', '#FDCC92']"
+    @complete="handleComplete"
+  >
+    <div class="text-8xl">🥳</div>
+  </ScratchToReveal>
+</template>
+
+<script lang="ts" setup>
+function handleComplete() {
+  // when scratch is completed do something
+}
+</script>
