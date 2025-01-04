49d8e38ee8f43bb0c7d91235fedb819c599ca029
whbbit1999 <luckyboy_dong@sina.com>
2025-01-04 22:19:55 +0800

    feat: Warp Background Component, forked from Magic UI.

---
+++ components/content/inspira/ui/warp-background/Beam.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    v-motion
+    :style="{
+      '--x': `${props.x}`,
+      '--width': `${props.width}`,
+      '--aspect-ratio': `${ar}`,
+      '--background': `linear-gradient(hsl(${hue} 80% 60%), transparent)`,
+    }"
+    class="absolute left-[var(--x)] top-0 translate-y-[100cqmax] [aspect-ratio:1/var(--aspect-ratio)] [background:var(--background)] [width:var(--width)]"
+    :initial="{
+      x: -50,
+      y: 200,
+    }"
+    :enter="{
+      x: -50,
+      y: -100,
+      transition: {
+        duration: props.duration,
+        delay: props.delay,
+        repeat: Infinity,
+        ease: 'linear',
+      },
+    }"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  width: string | number;
+  x: string | number;
+  delay: number;
+  duration: number;
+}
+const props = defineProps<Props>();
+
+const hue = computed(() => Math.floor(Math.random() * 360));
+const ar = computed(() => Math.floor(Math.random() * 10) + 1);
+</script>
+
+<style></style>
