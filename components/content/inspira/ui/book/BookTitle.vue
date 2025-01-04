42b73f1a347cf8b357f6c1c3c8c05052d31dc346
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-01-04 10:37:27 +0100

    feat: add 3D Book components

---
+++ components/content/inspira/ui/book/BookTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 :class="cn('font-bold select-none mt-3 mb-1 text-balance', $props.class)">
+    <slot />
+  </h1>
+</template>
+
+<script lang="ts" setup>
+import type { HTMLAttributes } from "vue";
+import { cn } from "~/lib/utils";
+
+interface BookTitleProps {
+  class?: HTMLAttributes["class"];
+}
+
+defineProps<BookTitleProps>();
+</script>
---
+++ components/content/inspira/ui/book/index.ts
@@ -0,0 +1,55 @@
+export const BOOK_RADIUS_MAP = {
+  sm: "rounded-sm",
+  md: "rounded-md",
+  lg: "rounded-lg",
+  xl: "rounded-xl",
+} as const;
+
+export const BOOK_SIZE_MAP = {
+  sm: { width: "180px", spineTranslation: "152px" },
+  md: { width: "220px", spineTranslation: "192px" },
+  lg: { width: "260px", spineTranslation: "232px" },
+  xl: { width: "300px", spineTranslation: "272px" },
+} as const;
+
+export const BOOK_SHADOW_SIZE_MAP = {
+  sm: "-5px 0 15px 5px var(--shadowColor)",
+  md: "-7px 0 25px 7px var(--shadowColor)",
+  lg: "-10px 0 35px 10px var(--shadowColor)",
+  xl: "-12px 0 45px 12px var(--shadowColor)",
+} as const;
+
+export const BOOK_COLOR_MAP = {
+  slate: { from: "from-slate-900", to: "to-slate-700" },
+  gray: { from: "from-gray-900", to: "to-gray-700" },
+  zinc: { from: "from-zinc-900", to: "to-zinc-700" },
+  neutral: { from: "from-neutral-900", to: "to-neutral-700" },
+  stone: { from: "from-stone-900", to: "to-stone-700" },
+  red: { from: "from-red-900", to: "to-red-700" },
+  orange: { from: "from-orange-900", to: "to-orange-700" },
+  amber: { from: "from-amber-900", to: "to-amber-700" },
+  yellow: { from: "from-yellow-900", to: "to-yellow-700" },
+  lime: { from: "from-lime-900", to: "to-lime-700" },
+  green: { from: "from-green-900", to: "to-green-700" },
+  emerald: { from: "from-emerald-900", to: "to-emerald-700" },
+  teal: { from: "from-teal-900", to: "to-teal-700" },
+  cyan: { from: "from-cyan-900", to: "to-cyan-700" },
+  sky: { from: "from-sky-900", to: "to-sky-700" },
+  blue: { from: "from-blue-900", to: "to-blue-700" },
+  indigo: { from: "from-indigo-900", to: "to-indigo-700" },
+  violet: { from: "from-violet-900", to: "to-violet-700" },
+  purple: { from: "from-purple-900", to: "to-purple-700" },
+  fuchsia: { from: "from-fuchsia-900", to: "to-fuchsia-700" },
+  pink: { from: "from-pink-900", to: "to-pink-700" },
+  rose: { from: "from-rose-900", to: "to-rose-700" },
+} as const;
+
+export type BookColor = keyof typeof BOOK_COLOR_MAP;
+export type BookSize = keyof typeof BOOK_SIZE_MAP;
+export type BookRadius = keyof typeof BOOK_RADIUS_MAP;
+export type BookShadowSize = keyof typeof BOOK_SHADOW_SIZE_MAP;
+
+export { default as Book } from "./Book.vue";
+export { default as BookHeader } from "./BookHeader.vue";
+export { default as BookTitle } from "./BookTitle.vue";
+export { default as BookDescription } from "./BookDescription.vue";
