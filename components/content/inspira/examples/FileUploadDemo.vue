67cfac2c94610611532324befa1cf77d4bc42839
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-01-04 20:24:27 +0100

    feat: add FileUploadDemo component

---
+++ components/content/inspira/examples/FileUploadDemo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="space-y-6 p-8 dark:bg-black">
+    <FileUpload class="rounded-lg border border-dashed border-neutral-200 dark:border-neutral-800">
+      <FileUploadGrid />
+    </FileUpload>
+  </div>
+</template>
