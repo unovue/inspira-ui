cbc911fa50e95069f82dcd0f5c0b2e1d7a9ba6f8
Reddy Uppathi <popvictoria@gmail.com>
2025-01-05 02:39:37 +0530

    feat: add warp background example component

---
+++ components/content/inspira/examples/warp-background/WarpBackgroundDemo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex h-screen items-center justify-center">
+    <WarpBackground>
+      <Card class="w-80">
+        <CardContent class="flex flex-col gap-2 pt-4">
+          <CardTitle>Congratulations on Your Promotion!</CardTitle>
+          <CardDescription class="text-sm text-muted-foreground">
+            Your hard work and dedication have paid off. We&apos;re thrilled to see you take this
+            next step in your career. Keep up the fantastic work!
+          </CardDescription>
+        </CardContent>
+      </Card>
+    </WarpBackground>
+  </div>
+</template>
+
+<!-- width: 100vh;
+    background: black;
+    height: 41vh; -->
