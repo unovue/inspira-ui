4542f72de6faf2146b8f0fef51e03ead6d33ca24
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-01-05 11:10:04 +0100

    feat: add file-tree components

---
+++ components/content/inspira/ui/file-tree/index.ts
@@ -0,0 +1,51 @@
+import type { HTMLAttributes } from "vue";
+
+export interface TreeProps {
+  class?: HTMLAttributes["class"];
+  initialSelectedId: string;
+  indicator?: boolean;
+  elements: TreeViewElement[];
+  initialExpandedItems: string[];
+  openIcon?: string;
+  closeIcon?: string;
+  fileIcon?: string;
+  direction?: "rtl" | "ltr";
+}
+
+export interface TreeContextProps {
+  selectedId: Ref<string | undefined>;
+  expandedItems: Ref<string[] | undefined>;
+  indicator: boolean;
+  openIcon: string;
+  closeIcon: string;
+  fileIcon: string;
+  direction: "rtl" | "ltr";
+  handleExpand: (id: string) => void;
+  selectItem: (id: string) => void;
+  setExpandedItems: (items: string[] | undefined) => void;
+}
+
+export interface TreeViewElement {
+  id: string;
+  name: string;
+  isSelectable?: boolean;
+  children?: TreeViewElement[];
+}
+
+export interface BaseItemProps {
+  class?: HTMLAttributes["class"];
+  id: string;
+  name: string;
+  isSelectable?: boolean;
+  isSelect?: boolean;
+}
+
+export interface FolderProps extends BaseItemProps {}
+
+export interface FileProps extends BaseItemProps {}
+
+export const TREE_CONTEXT_SYMBOL = Symbol("TREE_CONTEXT_SYMBOL");
+
+export { default as Tree } from "./Tree.vue";
+export { default as Folder } from "./Folder.vue";
+export { default as File } from "./File.vue";
