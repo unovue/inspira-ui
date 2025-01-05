a61e27fc33cc9db3eab7194da927acf3bd6eb5f4
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-01-05 07:49:16 +0100

    feat: add ScriptCopyDemo example component

---
+++ components/content/inspira/examples/ScriptCopyDemo.vue
@@ -0,0 +1,10 @@
+<template>
+  <ScriptCopy
+    :command-map="{
+      npm: 'npm run shadcn add button',
+      yarn: 'yarn shadcn add button',
+      pnpm: 'pnpm dlx shadcn@latest add button',
+      bun: 'bun x shadcn@latest add button',
+    }"
+  />
+</template>
