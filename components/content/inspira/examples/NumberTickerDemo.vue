494ee32cd8ec1f7e12226aa3055311479f6e639a
Stijn E <stijn.elskens@riskconcile.com>
2025-01-07 23:31:12 +0100

    fix(typo): correct className to class in NumberTickerDemo.vue

--- components/content/inspira/examples/NumberTickerDemo.vue
+++ components/content/inspira/examples/NumberTickerDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex min-h-64 items-center justify-center">
     <p
-      className="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-black dark:text-white"
+      class="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-black dark:text-white"
     >
       <NumberTicker :value="100" />
     </p>
