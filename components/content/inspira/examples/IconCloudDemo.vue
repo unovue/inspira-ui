3cc5d976a761e89bfab69c495d4bdecc5dd61207
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-01-08 19:35:32 +0100

    feat: add demo component

---
+++ components/content/inspira/examples/IconCloudDemo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="grid place-content-center p-6">
+    <IconCloud :images="imageUrls" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
+const imageUrls = slugs.map((slug) => `https://cdn.simpleicons.org/${slug}/${slug}`);
+</script>
