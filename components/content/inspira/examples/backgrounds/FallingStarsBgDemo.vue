b6ece8bbb2696dd5b84350eef59ca0b0cb760c52
Rahul Vashishtha <rahulvashistha96@gmail.com>
2025-01-09 01:51:27 +0530

    Made falling star bg support card mode.

--- components/content/inspira/examples/backgrounds/FallingStarsBgDemo.vue
+++ components/content/inspira/examples/backgrounds/FallingStarsBgDemo.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="relative flex h-96 flex-col items-center justify-center">
-    <FallingStarsBg class="bg-black" />
+    <FallingStarsBg
+      class="bg-white dark:bg-black"
+      :color="isDark ? '#FFF' : '#555'"
+    />
     <div class="z-[1] flex items-center">
-      <span class="text-6xl font-bold text-white">Inspira UI</span>
+      <span class="text-6xl font-bold text-black dark:text-white">Inspira UI</span>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const isDark = computed(() => useColorMode().value == "dark");
+</script>
