60d47d3841c4af791d3c4ec5d4e0fd3f5beadaad
Rahul Vashishtha <rahulvashistha96@gmail.com>
2025-01-09 01:05:12 +0530

    Fixed prettier

--- components/content/inspira/examples/NumberTickerDemo.vue
+++ components/content/inspira/examples/NumberTickerDemo.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex min-h-64 items-center justify-center">
-    <p
-      class="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-black dark:text-white"
-    >
+    <p class="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-black dark:text-white">
       <NumberTicker :value="100" />
     </p>
   </div>
