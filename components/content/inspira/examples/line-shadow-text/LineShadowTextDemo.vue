218a2eb7944055e9735f1ee9670ba286a2d38e02
Reddy Uppathi <popvictoria@gmail.com>
2025-01-13 18:00:38 +0530

    feat: add demo Line shadow text component

---
+++ components/content/inspira/examples/line-shadow-text/LineShadowTextDemo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-56 w-full flex-col items-center justify-center gap-2">
+    <h1 class="text-balance text-8xl font-extrabold leading-none tracking-tighter">
+      Ship
+      <LineShadowText
+        class="italic"
+        :shadow-color="shadowColor"
+      >
+        Fast
+      </LineShadowText>
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isDark = computed(() => useColorMode().value == "dark");
+const shadowColor = computed(() => (isDark.value ? "white" : "black"));
+</script>
