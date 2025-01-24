a0cf3cf759dc67ca5cee71605eca2b331fae9817
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-01-24 10:14:35 +0100

    refactor: replace templateRef with useTemplateRef in the 'compare' component

--- components/content/inspira/ui/compare/Compare.vue
+++ components/content/inspira/ui/compare/Compare.vue
@@ -161,7 +161,7 @@ const emit = defineEmits<{
   (e: "hover:leave"): void;
 }>();
 
-const sliderRef = templateRef<HTMLDivElement | null>("sliderRef");
+const sliderRef = useTemplateRef<HTMLDivElement | null>("sliderRef");
 const sliderXPercent = ref(props.initialSliderPercentage);
 const isDragging = ref(false);
 const isMouseOver = ref(false);
