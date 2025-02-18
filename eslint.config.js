35c339807eef07c38e321c388fb972f901201aba
Rahul Vashishtha <rahulvashistha96@gmail.com>
2025-02-19 03:09:22 +0530

    fix: Disabled multi root rule

--- eslint.config.js
+++ eslint.config.js
@@ -54,6 +54,7 @@ export default withNuxt(
       "func-style": ["error", "declaration"],
       "vue/multi-word-component-names": "off",
       "vue/require-default-prop": "off",
+      "vue/no-multiple-template-root": "off",
       "vue/block-order": [
         "error",
         {
