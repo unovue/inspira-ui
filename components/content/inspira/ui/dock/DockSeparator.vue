70b180ed37d5b302f310aff2948443dee0f66dd4
LeeChan <2535849195@qq.com>
2025-03-04 23:52:17 +0800

    feat: Add Vertical Dock

---
+++ components/content/inspira/ui/dock/DockSeparator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    :class="
+      cn('relative block bg-secondary', orientation === 'vertical' ? 'w-4/5 h-0.5' : 'h-4/5 w-0.5')
+    "
+  ></div>
+</template>
+
+<script setup lang="ts">
+import { ORIENTATION_INJECTION_KEY } from "./injectionKeys";
+import { cn } from "~/lib/utils";
+
+const orientation = inject(ORIENTATION_INJECTION_KEY, "vertical");
+</script>
