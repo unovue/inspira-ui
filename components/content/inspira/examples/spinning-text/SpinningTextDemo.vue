9aa288ded97da087a1061bed7ca91d84c0126259
whbbit1999 <luckyboy_dong@sina.com>
2025-03-23 11:47:43 +0800

    feat: spinning-text component

---
+++ components/content/inspira/examples/spinning-text/SpinningTextDemo.vue
@@ -0,0 +1,10 @@
+<template>
+  <ClientOnly>
+    <div class="flex min-h-[350px] w-full items-center justify-center">
+      <SpinningText text="learn more · earn more · grow more ·" />
+    </div>
+  </ClientOnly>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped></style>
