51e0ac5b5d0199ef498918bcde28a81dca5d7f00
Rahul Vashishtha <rahulvashistha96@gmail.com>
2025-08-30 01:21:36 +0530

    feat: :rocket: Added <SleekLineCursor /> component

---
+++ components/content/inspira/examples/sleek-line-cursor/SleekLineCursorDemo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center justify-center text-xl min-h-96 w-full h-96 relative">
+    <span class="hidden md:block">Move your mouse around</span>
+    <span class="block md:hidden">Swipe anywhere to see the cursor</span>
+    <ClientOnly>
+      <SleekLineCursor />
+    </ClientOnly>
+  </div>
+</template>
