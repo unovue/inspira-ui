dd86144f6b425c8c3572a6fb1fb16ec971cee4d2
Rahul Vashishtha <rahulvashistha96@gmail.com>
2025-10-13 05:00:54 +0530

    Added formatter and linter with oxc and rolldown

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,22 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/i18n'],
+  modules: ["@nuxtjs/i18n", "@nuxt/eslint"],
   i18n: {
-    defaultLocale: 'en',
-    locales: [{
-      code: 'en',
-      name: 'English',
-    }, {
-      code: 'fr',
-      name: 'Français',
-    }],
+    defaultLocale: "en",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+      },
+      {
+        code: "fr",
+        name: "Français",
+      },
+    ],
   },
-})
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
