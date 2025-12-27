d7115d695076f7b7caee0445bd991a072a01956f
Rahul Vashishtha <rahulvashistha96@gmail.com>
2025-12-27 22:40:37 +0530

    Updated changelog

--- app/components/common/changelog/InspiraChangelog.vue
+++ app/components/common/changelog/InspiraChangelog.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 const versions = ref([
   {
+    title: "Released v3: Dec 23, 2025",
+    description: "Migrated docs to Docus.dev and improved docs user experience.",
+    date: "2025-12-27",
+    ui: {
+      container: "max-w-md",
+    },
+  },
+  {
     title: "Bug Fixes: Dec 23, 2025",
     description: "Performance updates in Github Globe and Falling Stars Background.",
-    date: "2025-04-27",
+    date: "2025-12-23",
     ui: {
       container: "max-w-md",
     },
