4448dff025f1f04ea84bdf2dbdc98ba9b7fa70a8
Rahul Vashishtha <rahulvashistha96@gmail.com>
2025-12-30 19:43:44 +0530

    Updated glare card

--- app/components/inspira/examples/glare-card/GlareCardDemo.vue
+++ app/components/inspira/examples/glare-card/GlareCardDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center">
     <GlareCard class="flex flex-col items-center justify-center">
-      <img src="/logo-dark.svg" />
+      <img src="https://cdn.inspira-ui.com/logo-dark.svg" />
       <p class="mt-4 text-xl font-bold text-white">Inspira UI</p>
     </GlareCard>
   </div>
