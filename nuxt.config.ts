db389077d286742eaa1b35f965009f2984ff130b
Rahul Vashishtha <rahulvashistha96@gmail.com>
2026-09-02 11:11:42 +0530

    Updated font in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -140,7 +140,7 @@ export default defineNuxtConfig({
     processCSSVariables: true,
     families: [
       {
-        name: "Geist",
+        name: "Inter",
         provider: "google",
         global: true,
       },
